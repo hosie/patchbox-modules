b4a4ecc587ca21de743de8b356b15fd03c267b50
John Hosie <hosiemail@gmail.com>
2023-03-26 15:52:36 +0100

    program change

---
+++ program-change/install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "happy padding"
---
+++ program-change/launch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+. /usr/local/pisound/scripts/common/start_puredata.sh
+start_puredata /usr/local/patchbox-modules/imported/program-change/main.pd @
